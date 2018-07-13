b48ebcdd5a178ad425909a1e1d46244fd1e29192
Caio Garcia <garcia.caio@gmail.com>
2018-07-13 16:40:23 -0300

    Razor pages not complete

--- myrazorapp/Pages/Index.cshtml.cs
+++ myrazorapp/Pages/Index.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
+//Video pausado em 0:36
 namespace myrazorapp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private readonly AppDbContext _db;
+
+        public IndexModel(AppDbContext db){
+            this._db = db;
+        }
+
+        //Devia ser NÃO Estatica
+        public static IList<Customer> Customers { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Customers = await _db.Customers.AsNoTracking().ToListAsync();
         }
     }
 }
