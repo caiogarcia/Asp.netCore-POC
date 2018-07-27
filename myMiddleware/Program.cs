e2f8a6e4cbed92e89740d39b6599b6eef295497c
Caio Garcia <garcia.caio@gmail.com>
2018-07-27 08:39:01 -0300

    My Middleware exemple

---
+++ myMiddleware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myMiddleware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
